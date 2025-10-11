24c96262c0353a17014e66921b9d4bc10ff5e5b3
Conor Heffron <conor.heffron@gmail.com>
2025-10-12 00:56:00 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Build the Go program
+echo "Building the Go program..."
+go build -o go-world
+
+# Run the program with a runtime argument
+echo "Running the program..."
+./go-world -name="Conor Heffron"
