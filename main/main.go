40f66a9e630073a45187af3ce1f11b12ae7c5aeb
Conor Heffron <conor.heffron@gmail.com>
2025-10-12 00:49:13 +0100

    Update and rename HelloWorld.go to main.go

---
+++ main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"go-world/parser"
+)
+
+func main() {
+	// Parse the name argument using the parser package
+	name := parser.ParseName()
+
+	// Print the greeting
+	fmt.Printf("Hello there, %s!\n", name)
+}
