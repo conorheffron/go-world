f6054d549e50bebb867f1ebc4c86bcdf85e246a6
Conor Heffron <conor.heffron@gmail.com>
2025-10-12 01:05:22 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "$(ls -ltr)"
+
 # Build the Go program
 echo "Building the Go program..."
-go build ./... -o go-world
+go build -o go-world ./...
+
+echo "$(ls -ltr)"
 
 # Run the program with a runtime argument
 echo "Running the program..."
 ./go-world -name="Conor Heffron"
+
+./...
