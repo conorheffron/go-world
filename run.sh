6caea63d5f9b815c2e06a732781a2ec356679f1a
Conor Heffron <conor.heffron@gmail.com>
2025-10-12 01:01:00 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Build the Go program
 echo "Building the Go program..."
-go build -o go-world
+go build ./... -o go-world
 
 # Run the program with a runtime argument
 echo "Running the program..."
